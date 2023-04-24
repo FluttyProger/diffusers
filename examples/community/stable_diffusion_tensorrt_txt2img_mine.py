a7117647780e85dbfc700b880d16f81b805befd3
FluttyProger <ktaraso@yandex.ru>
2023-04-24 18:31:56 +0300

    Update stable_diffusion_tensorrt_txt2img_mine.py

--- examples/community/stable_diffusion_tensorrt_txt2img_mine.py
+++ examples/community/stable_diffusion_tensorrt_txt2img_mine.py
@@ -122,8 +122,10 @@ class Engine:
 
         config_kwargs = {}
 
-        config_kwargs["preview_features"] = [trt.PreviewFeature.FASTER_DYNAMIC_SHAPES_0805, trt.PreviewFeature.DISABLE_EXTERNAL_TACTIC_SOURCES_FOR_CORE_0805]
-        
+        config_kwargs["preview_features"] = [trt.PreviewFeature.DISABLE_EXTERNAL_TACTIC_SOURCES_FOR_CORE_0805]
+        if enable_preview:
+            # Faster dynamic shapes made optional since it increases engine build time.
+            config_kwargs["preview_features"].append(trt.PreviewFeature.FASTER_DYNAMIC_SHAPES_0805)
         if workspace_size > 0:
             config_kwargs["memory_pool_limits"] = {trt.MemoryPoolType.WORKSPACE: workspace_size}
         if not enable_all_tactics:
